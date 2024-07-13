4522466a2a0788560e0fab622cb20abfffee0b01
eerkaijun <kaijuneer@gmail.com>
2024-07-13 13:27:25 +0000

    move helios to example directory

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # sp1-helios
+
+### Getting Started
+
+To run an example helios light client, run
+```sh
+cd program
+cargo run --package program --example helios
+```
+
+To run the proving program, run
+```sh
+cd program
+cargo run
+```
--- program/src/main.rs
+++ program/src/main.rs
@@ -1,9 +1,5 @@
-use std::{path::PathBuf, str::FromStr, env};
-
 use dotenv::dotenv;
-use ethers::{types::Address, utils};
 use eyre::Result;
-use helios::{client::ClientBuilder, config::networks::Network, prelude::*, types::BlockTag};
 use tracing::info;
 use tracing_subscriber::FmtSubscriber;
 
@@ -15,28 +11,7 @@ async fn main() -> Result<()> {
     let subscriber = FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber)?;
 
-    let untrusted_rpc_url = env::var("UNTRUSTED_RPC_URL")?;
-    info!("Untrusted RPC URL: {}", untrusted_rpc_url);
-
-    // initialize helios client
-    let mut client: helios::client::Client<FileDB> = ClientBuilder::new()
-        .network(Network::MAINNET)
-        .consensus_rpc("https://www.lightclientdata.org")
-        .execution_rpc(&untrusted_rpc_url)
-        .load_external_fallback()
-        .data_dir(PathBuf::from("/tmp/helios"))
-        .build()?;
-
-    client.start().await?;
-    client.wait_synced().await;
-
-    let head_block_num = client.get_block_number().await?;
-    let addr = Address::from_str("0x00000000219ab540356cBB839Cbe05303d7705Fa")?;
-    let block = BlockTag::Latest;
-    let balance = client.get_balance(&addr, block).await?;
-
-    info!("synced up to block: {}", head_block_num);
-    info!("balance of deposit contract: {}", utils::format_ether(balance));
+    info!("Generating proofs...");
 
     Ok(())
 }
